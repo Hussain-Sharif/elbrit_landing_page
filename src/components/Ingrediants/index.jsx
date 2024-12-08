7822d7843e2ae48d94c79191deef6b7d4aed0571
Hussain-Sharif <99sharif00@gmail.com>
2024-12-08 15:31:20 +0530

    small changes in ingresients section

--- src/components/Ingrediants/index.jsx
+++ src/components/Ingrediants/index.jsx
@@ -3,7 +3,7 @@ import { ReUseButton, ReUseDiv, ReUseImage, ReUseText } from "../../reusableStyl
 
 export const Ingrediants=()=>(
     <ReUseDiv w="100%" display="flex" fD="column" jC="center" aI="center">
-        <ReUseDiv w="85%"  display="flex" fD="row" fW="wrap" jC="space-between" aI="center">
+        <ReUseDiv w="85%"  display="flex" fD="row" fW="wrap" jC="space-between" aI="flex-start">
             <ReUseDiv m="15px 1px"  minW="300px" maxW="540px" display="flex" fD="row"  jC="space-between" aI="flex-start">
                 <ReUseDiv   w="100%" wB="100%" maxW="320px" display="flex" fD="column" jC="flex-start" aI="flex-start"> 
                     <ReUseText fs="15px" fw="bolder" color="#003569">INGREDIENTS</ReUseText>
@@ -23,7 +23,7 @@ export const Ingrediants=()=>(
                 <ReUseButton color="#003569" fw="bold" tD="underline" border="none"  bg="transparent" >SEE MORE</ReUseButton>
             </ReUseDiv>
 
-            <ReUseDiv m="15px 0px"  minW="320px" minH="200px" maxW="540px" display="flex" fD="column" jC="space-between" aI="flex-start" p="15px" bgSize="cover" br="16px" bgImgUrl="https://s3-alpha-sig.figma.com/img/b67d/ea77/c211f1eeb242f8ddce11f7afa3e49063?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bvn7niZI~ZoCC4yF1tx4KZk6tXeUfJVvteJGhMWd2g6fCZmt7oKFxBs9kCM4GdJsC9wYbHdzJQnO2IckVZ2JOeFliu0~IokNny-8s1WmmIJB1cHNBbrkUZqa5lORxBwUMbdJ94c2X-nXlZiMGKfLHrUWQQ2WbCNq4XlPW0~snpphxryOBKfS26XhGLhSu8pVx5tbDD-iKHzeXhDZXEyoE4vTrTTmxuvmr8K-NF9WpwSMmyg16VOWm-huoQNIlyaQIWE83lpyKQmO85NOq-eAR1reMuqSEpiHnSo~Fo52ka056fhKEr9iAgcHB1pvlnRum676gKgYRe4qHEGxtAIQSw__" >
+            <ReUseDiv m="15px 0px"  minW="320px" minH="200px" w="100%" wB="auto" maxW="540px" display="flex" fD="column" jC="space-between" aI="flex-start" p="15px" bgSize="cover" br="16px" bgImgUrl="https://s3-alpha-sig.figma.com/img/b67d/ea77/c211f1eeb242f8ddce11f7afa3e49063?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=bvn7niZI~ZoCC4yF1tx4KZk6tXeUfJVvteJGhMWd2g6fCZmt7oKFxBs9kCM4GdJsC9wYbHdzJQnO2IckVZ2JOeFliu0~IokNny-8s1WmmIJB1cHNBbrkUZqa5lORxBwUMbdJ94c2X-nXlZiMGKfLHrUWQQ2WbCNq4XlPW0~snpphxryOBKfS26XhGLhSu8pVx5tbDD-iKHzeXhDZXEyoE4vTrTTmxuvmr8K-NF9WpwSMmyg16VOWm-huoQNIlyaQIWE83lpyKQmO85NOq-eAR1reMuqSEpiHnSo~Fo52ka056fhKEr9iAgcHB1pvlnRum676gKgYRe4qHEGxtAIQSw__" >
                 <ReUseDiv>
                     <ReUseText color="#2D3F59" fs="24px" fw="bold">Vitamin B3 <br/></ReUseText>
                     <ReUseText Color="#727272" fs="15px" fw="bold">Niacin for healthy gut and skin</ReUseText>
@@ -31,7 +31,7 @@ export const Ingrediants=()=>(
                 <ReUseButton color="#003569" fw="bold" tD="underline" border="none"  bg="transparent" >SEE MORE</ReUseButton>
             </ReUseDiv>
             
-            <ReUseDiv m="15px 0px" minW="320px" minH="200px" maxW="540px" display="flex" fD="column" jC="space-between" aI="flex-start" p="15px" bgSize="cover" br="16px" bgImgUrl="https://s3-alpha-sig.figma.com/img/83aa/9334/94616bd5d5a4bedd72a970d56dfe6b3d?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QTE1vWwId4dF7m8BzGeTMSMII3pPAv1IylQnBZ7VET~Ymu-6RCBfHfd4~MLasTYfzoNMs4oujdeFS5AWukI6Wihe6qbpIJ-M6uw6Xm0lixAcc9P6R4~8xQCTLaCPlDDM6rQJWSTQk3aCeGGynMB3vxlc1KOMQp36DLFSSF7~GUP5al8vgCVYK7u8nT6qkF2AHQxn6mAiaZ97nuOE3m3axFuiN2RssSlK1jXbc5SyOncyN0eaiqKQR6XzWqO5J14JKHzrxTSWuPE7S8WxCqib8-r-5A9TqJMTciojn65f8dHtJs0HtMQkjvrrbbrpDOZbFmgX5rn-nduIrkliLiW2Kg__" >
+            <ReUseDiv m="15px 0px" minW="320px" minH="200px" w="100%" wB="auto" maxW="540px" display="flex" fD="column" jC="space-between" aI="flex-start" p="15px" bgSize="cover" br="16px" bgImgUrl="https://s3-alpha-sig.figma.com/img/83aa/9334/94616bd5d5a4bedd72a970d56dfe6b3d?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=QTE1vWwId4dF7m8BzGeTMSMII3pPAv1IylQnBZ7VET~Ymu-6RCBfHfd4~MLasTYfzoNMs4oujdeFS5AWukI6Wihe6qbpIJ-M6uw6Xm0lixAcc9P6R4~8xQCTLaCPlDDM6rQJWSTQk3aCeGGynMB3vxlc1KOMQp36DLFSSF7~GUP5al8vgCVYK7u8nT6qkF2AHQxn6mAiaZ97nuOE3m3axFuiN2RssSlK1jXbc5SyOncyN0eaiqKQR6XzWqO5J14JKHzrxTSWuPE7S8WxCqib8-r-5A9TqJMTciojn65f8dHtJs0HtMQkjvrrbbrpDOZbFmgX5rn-nduIrkliLiW2Kg__" >
                 <ReUseDiv>
                     <ReUseText color="#2D3F59" fs="24px" fw="bold">Magnesium<br/></ReUseText>
                     <ReUseText Color="#727272" fs="15px" fw="bold">Boost energy and support muscle function</ReUseText>
@@ -39,7 +39,7 @@ export const Ingrediants=()=>(
                 <ReUseButton color="#003569" fw="bold" tD="underline" border="none"  bg="transparent" >SEE MORE</ReUseButton>
             </ReUseDiv>
 
-            <ReUseDiv m="15px 0px" minW="320px" minH="200px" maxW="540px" display="flex" fD="column" jC="space-between" aI="flex-start" p="15px" bgSize="cover" br="16px" bgImgUrl="https://s3-alpha-sig.figma.com/img/bb9b/f61c/acecf67b63c3bb4c36ba72caf74dc92d?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kH3DUUkt5bYIq-xIGST4QEgvWCPHloR7rQ6gzpepsk~hSc2zgrd8IyvxMQ2JRXJ~KmimI9vzSD9unzhZpY-zvT~R5r5fovr4QLWzGSun76XSiK0HZob05tUOPZj59zChUKF9mwE4U2cUG0VGwEZ1pDOevs10TygZND7Mow33bk9jZZB7stnu6Kx9Fq7gPFI4yd-xsj64mkA4DNgLq5tyn-nfM3WzaEMe2PFgsBoTCNJdNMiaEnelDSzyCkzF3xo-x5guR3fJBUPpE3eXYYPv8SnNPo4EW9dwX5xxbsAv6F7bZxkR9OJNM-c9pSjubsOOc22VxkaXbQk~oaccetzMpg__" >
+            <ReUseDiv m="15px 0px" minW="320px" minH="200px" w="100%" wB="auto" maxW="540px" display="flex" fD="column" jC="space-between" aI="flex-start" p="15px" bgSize="cover" br="16px" bgImgUrl="https://s3-alpha-sig.figma.com/img/bb9b/f61c/acecf67b63c3bb4c36ba72caf74dc92d?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=kH3DUUkt5bYIq-xIGST4QEgvWCPHloR7rQ6gzpepsk~hSc2zgrd8IyvxMQ2JRXJ~KmimI9vzSD9unzhZpY-zvT~R5r5fovr4QLWzGSun76XSiK0HZob05tUOPZj59zChUKF9mwE4U2cUG0VGwEZ1pDOevs10TygZND7Mow33bk9jZZB7stnu6Kx9Fq7gPFI4yd-xsj64mkA4DNgLq5tyn-nfM3WzaEMe2PFgsBoTCNJdNMiaEnelDSzyCkzF3xo-x5guR3fJBUPpE3eXYYPv8SnNPo4EW9dwX5xxbsAv6F7bZxkR9OJNM-c9pSjubsOOc22VxkaXbQk~oaccetzMpg__" >
                 <ReUseDiv>
                     <ReUseText color="#2D3F59" fs="24px" fw="bold">Hyaluronic Acid<br/></ReUseText>
                     <ReUseText Color="#727272" fs="15px" fw="bold">For smooth, supple and soft skin!</ReUseText>
@@ -47,7 +47,7 @@ export const Ingrediants=()=>(
                 <ReUseButton color="#003569" fw="bold" tD="underline" border="none"  bg="transparent" >SEE MORE</ReUseButton>
             </ReUseDiv>
 
-            <ReUseDiv m="15px 0px" minW="320px" minH="200px" maxW="540px" display="flex" fD="column" jC="space-between" aI="flex-start" p="15px" bgSize="cover" br="16px" bgImgUrl="https://s3-alpha-sig.figma.com/img/9f1f/4b6a/35f9c69109d60d6aacc825461a966f41?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=P4QeSzEx6Qk1lfrSnu5oaMPU1nEty3luQ4uWZ547yeSRhsphPVMJdfyRTa2XsnTT5gVy2xQT9xeUp9NOblWzcNpiCuSRAiL8Em~~PuoT85H0aaAFzHv-adJEfJMJ9rxxysSmwK1S8P5y1ZGc2BC9QjRWBxkisYVCmtiLnqr~O-QF2HaSavzseoV-1aQlHtkd9RUICqCC0jy0~VP0-xJV~xKtEzTWHAWgUND8EEVS34OBL3CYOZVMxxrAvGJ2vfzTdTYZisruw958TW1TEDvG6~pXZCc-ZQLlM3fv1l3WcE-OUW2UB3pfshubdFNjhsGpf2Vzcj7G-YXlHXl-zXC67Q__" >
+            <ReUseDiv m="15px 0px" minW="320px" minH="200px" w="100%" wB="auto" maxW="540px" display="flex" fD="column" jC="space-between" aI="flex-start" p="15px" bgSize="cover" br="16px" bgImgUrl="https://s3-alpha-sig.figma.com/img/9f1f/4b6a/35f9c69109d60d6aacc825461a966f41?Expires=1734307200&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=P4QeSzEx6Qk1lfrSnu5oaMPU1nEty3luQ4uWZ547yeSRhsphPVMJdfyRTa2XsnTT5gVy2xQT9xeUp9NOblWzcNpiCuSRAiL8Em~~PuoT85H0aaAFzHv-adJEfJMJ9rxxysSmwK1S8P5y1ZGc2BC9QjRWBxkisYVCmtiLnqr~O-QF2HaSavzseoV-1aQlHtkd9RUICqCC0jy0~VP0-xJV~xKtEzTWHAWgUND8EEVS34OBL3CYOZVMxxrAvGJ2vfzTdTYZisruw958TW1TEDvG6~pXZCc-ZQLlM3fv1l3WcE-OUW2UB3pfshubdFNjhsGpf2Vzcj7G-YXlHXl-zXC67Q__" >
                 <ReUseDiv>
                     <ReUseText color="#2D3F59" fs="24px" fw="bold">Lactobacillus<br/></ReUseText>
                     <ReUseText Color="#727272" fs="15px" fw="bold">Invigorate your gut microbiome</ReUseText>
